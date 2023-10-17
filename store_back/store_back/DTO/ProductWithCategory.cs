4069e929739094c1e99e47aad45b8bb617b4cb14
salma <122889996+salmaaSaamir@users.noreply.github.com>
2023-10-17 13:27:48 +0300

    Add files via upload

---
+++ store_back/store_back/DTO/ProductWithCategory.cs
@@ -0,0 +1,11 @@
+﻿namespace store_back.DTO
+{
+    public class ProductWithCategory
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public float price { get; set; }
+        public string   Image { get; set; }
+        public string CategoryName { get; set; }    
+    }
+}
